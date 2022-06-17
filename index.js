84e6ab76a6b853ba997437f733a23ee849679187
verah <verahnyagaka101@gmail.com>
2022-06-17 11:24:44 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -36,27 +36,27 @@ Stop
 */
 
 // You can run `node index.js` to view these console logs
-// if (require.main === module) {
-//   // add your own custom tests in here
-//   console.log("Expecting: true");
-//   console.log("=>", isPalindrome("racecar"));
+if (require.main === module) {
+  // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
 
-//   console.log("");
+  console.log("");
 
-//   console.log("Expecting: false");
-//   console.log("=>", isPalindrome("robot"));
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
 
-//   console.log("");
+  console.log("");
   
 
-//   console.log("Expecting: true");
-//   console.log("=>", isPalindrome("mom"));
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
 
-//   console.log("");
+  console.log("");
 
-//   console.log("Expecting: false");
-//   console.log("=>", isPalindrome("cup"));
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("cup"));
 
-// }
+}
 
 module.exports = isPalindrome;
